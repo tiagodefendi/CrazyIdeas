14bd719ae60c7a0349af12fb34c927c5804af68c
Defendi <tiagodefendidasilva@gmail.com>
2024-11-13 12:38:30 -0300

    config

--- src/main/java/com/defendi/crazyideas/CrazyIdeas.java
+++ src/main/java/com/defendi/crazyideas/CrazyIdeas.java
@@ -10,7 +10,9 @@ import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -20,7 +22,7 @@ import org.slf4j.Logger;
 @Mod(CrazyIdeas.MOD_ID)
 public class CrazyIdeas {
     public static final String MOD_ID = "crazyideas";
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public CrazyIdeas() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -34,6 +36,9 @@ public class CrazyIdeas {
         MinecraftForge.EVENT_BUS.register(this);
 
         modEventBus.addListener(this::addCreative);
+
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
