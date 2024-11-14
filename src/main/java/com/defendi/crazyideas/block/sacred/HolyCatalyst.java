aed13ec24a79505de50b3fc9ff1e569be6704170
Defendi <tiagodefendidasilva@gmail.com>
2024-11-14 11:18:23 -0300

    Create HolyCatalyst.java

---
+++ src/main/java/com/defendi/crazyideas/block/sacred/HolyCatalyst.java
@@ -0,0 +1,9 @@
+package com.defendi.crazyideas.block.sacred;
+
+import net.minecraft.world.level.block.Block;
+
+public class HolyCatalyst extends Block {
+    public HolyCatalyst(Properties properties) {
+        super(properties);
+    }
+}
