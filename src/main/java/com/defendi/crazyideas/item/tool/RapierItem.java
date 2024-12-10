2b892344cd67611da9e6889567544569cb6df241
Defendi <tiagodefendidasilva@gmail.com>
2024-12-10 17:27:47 -0300

    Finished Rapier

--- src/main/java/com/defendi/crazyideas/item/tool/RapierItem.java
+++ src/main/java/com/defendi/crazyideas/item/tool/RapierItem.java
@@ -2,12 +2,12 @@ package com.defendi.crazyideas.item.tool;
 
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 
 public class RapierItem extends SwordItem {
     public RapierItem(Tier pTier, Properties pProperties) {
@@ -20,11 +20,16 @@ public class RapierItem extends SwordItem {
 
         pPlayer.getCooldowns().addCooldown(this, 60);
 
-        if(!pPlayer.getBlockStateOn().isAir()) {
-            pPlayer.jumpFromGround();
-        }
+        pPlayer.setDeltaMovement(
+                pPlayer.getLookAngle().multiply(
+                        -0.75,
+                        0,
+                        -0.75)
+        );
 
-        pPlayer.move(MoverType.SELF, pPlayer.getLookAngle().multiply(-1.5f, 0, -1.5f));
+        if(!pPlayer.getBlockStateOn().isAir()) {
+            pPlayer.addDeltaMovement(Vec3.ZERO.add(0, 1.0/3,0));
+        }
 
         return InteractionResultHolder.consume(itemstack);
     }
